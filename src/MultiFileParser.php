204b69862e6d77ca4fb24e2717c9a09adc791ace
Dennis Snell <dmsnell@xkq.io>
2023-09-25 16:00:30 -0700

    Initial commit

---
+++ src/MultiFileParser.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Dmsnell;
+
+require( __DIR__ . '/class-wp-block-parser-block.php' );
+require( __DIR__ . '/class-wp-block-parser-frame.php' );
+require( __DIR__ . '/class-wp-block-parser.php' );
---
+++ tests/MultiFileParserBench.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Dmsnell\Tests\Benchmark;
+
+use Dmsnell;
+
+require( __DIR__ . '/../src/MultiFileParser.php');
+
+class MultiFileParserBench {
+    private $doc = null;
+
+    public function __construct() {
+        $this->doc = file_get_contents( __DIR__ . '/../documents/early-adopting-the-future.html' );
+    }
+
+    public function benchEmpty() {
+        for ( $i = 0; $i < 3000; $i++ ) {
+            $parser = new Dmsnell\WP_Block_Parser();
+            $parser->parse( '' );
+        }
+    }
+
+    public function benchMany() {
+        for ( $i = 0; $i < 3000; $i++ ) {
+            $parser = new Dmsnell\WP_Block_Parser();
+            $parser->parse( $this->doc );
+        }
+    }
+
+    public function benchSingleton() {
+        $parser = new Dmsnell\WP_Block_Parser();
+
+        for ( $i = 0; $i < 3000; $i++ ) {
+            $parser->parse( $this->doc );
+        }
+    }
+}
